803e81744ad8da48b7ea5c628cf2034bd7233bba
Muhammad AMirul Muchminiin <121718601+amirul110@users.noreply.github.com>
2025-05-02 22:32:37 +0700

    Add files via upload

---
+++ HTML Form/3-terima-data.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <h2>Data Diterima</h2>
+    <p>Nama: <?php echo $_GET["nama"]; ?></p>
+    <p>Email: <?php echo $_GET["email"]; ?></p>
+    <p>Telepon: <?php echo $_GET["telepon"]; ?></p>
+</body>
+</html>
