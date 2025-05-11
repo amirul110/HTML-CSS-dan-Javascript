7569be12e7c963935c566b096f0d8a01746accf7
Muhammad AMirul Muchminiin <121718601+amirul110@users.noreply.github.com>
2025-05-11 07:59:59 +0700

    Create 6-operator-perbandingan.js

---
+++ Javascript Dasar/6-operator-perbandingan.js
@@ -0,0 +1,16 @@
+let jam = 10
+let sepatu = 2
+let kucing = 10
+let ayam = "10"
+
+let hasil = jam > sepatu
+console.log(hasil)
+
+let kacamata = jam < sepatu
+console.log(kacamata)
+
+let gajah = jam === "ayam"
+console.log(gajah) //false beda tipe data
+
+let ikan = jam !== kucing
+console.log(ikan)
