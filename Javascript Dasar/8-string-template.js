3f01d5413ff09d602b6f47759d1c45ddaa19bf9b
Muhammad AMirul Muchminiin <121718601+amirul110@users.noreply.github.com>
2025-05-11 08:02:04 +0700

    Create 8-string-template.js

---
+++ Javascript Dasar/8-string-template.js
@@ -0,0 +1,33 @@
+let motor = "selamat";
+let sapi = "pagi";
+
+let hasil = `Name : ${motor} ${sapi}`;
+console.log(hasil); // Name : selamat pagi
+
+
+let angka1 = 5;
+let angka2 = 10;
+console.log(`Hasil: ${angka1 + angka2}`); // Output: "Hasil: 15"
+
+const nama = "amirul";
+const pesawat = 90;
+
+const result = `Name : ${nama}, Lulus : ${pesawat > 74}`;
+console.log(result); //Name : amirul, Lulus : true
+
+
+
+let multilineString =
+`Nama saya adalah
+muhammad amirul muchminiin
+hello dunia
+test bang
+`;
+
+console.log(multilineString) // output akan sama persis dengan penulisan di kode
+/*output 
+Nama saya adalah
+muhammad amirul muchminiin
+hello dunia
+test bang
+*/
