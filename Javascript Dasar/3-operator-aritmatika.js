faaceac8284633c201868be3327b16883a0269bc
Muhammad AMirul Muchminiin <121718601+amirul110@users.noreply.github.com>
2025-05-11 07:59:00 +0700

    Create 3-operator-aritmatika.js

---
+++ Javascript Dasar/3-operator-aritmatika.js
@@ -0,0 +1,13 @@
+let pisang = 10
+let mangga = 3
+
+
+let anggur = pisang + mangga;
+console.log(anggur)
+
+let pepaya = pisang % mangga;
+console.log(pepaya)
+
+// 10 x 10 x10 
+let melon = pisang ** mangga;
+console.log(melon)
