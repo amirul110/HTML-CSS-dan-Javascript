0d51163019fe42f5666035de296be2099c7c68f7
Muhammad AMirul Muchminiin <121718601+amirul110@users.noreply.github.com>
2025-05-11 07:58:28 +0700

    Create 2-escape-sequence.js

---
+++ Javascript Dasar/2-escape-sequence.js
@@ -0,0 +1,20 @@
+console.log("halo dunia")
+console.log("halo  \n dunia\n")
+console.log("halo dunia")
+
+var fullName;
+
+fullName = "amirul"
+
+fullName = "batu bara"
+
+fullName = "joko andrian"
+console.log(fullName)
+
+let namaPertama = "muhammad amirul "
+let namaKedua = "muchminiin"
+let fullName = namaPertama + namaKedua
+let namaLengkap = "muhammad Amirul" + " " + "muchminiin";
+console.log(fullName)
+
+const aplikasi = "belajar machine learning"
