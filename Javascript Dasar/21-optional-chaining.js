c237a86d3465bb56fa9fb854bc3b5c565b4b30cf
Muhammad AMirul Muchminiin <121718601+amirul110@users.noreply.github.com>
2025-05-12 07:23:22 +0700

    Create 21-optional-chaining.js

---
+++ Javascript Dasar/21-optional-chaining.js
@@ -0,0 +1,4 @@
+let person = null
+
+// digunakan untuk mengecek tipe propery bertipe null atau undefined
+console.log(person?.addres?.country) //?. selalu mengembalikan undefined ketika menemui null/undefined
