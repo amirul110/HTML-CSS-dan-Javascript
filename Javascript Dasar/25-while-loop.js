99b01bcb2662523935caddd0fe40956533a6182a
Muhammad AMirul Muchminiin <121718601+amirul110@users.noreply.github.com>
2025-05-13 06:40:43 +0700

    Create 25-while-loop.js

---
+++ Javascript Dasar/25-while-loop.js
@@ -0,0 +1,20 @@
+let baca = 1
+while(baca < 10) { // jika salah kondisi diawal,tidak akan jalankan perulangan 1x
+    console.log(`Perulangan ke ${baca}`)
+    baca++
+}
+ // cocok untuk perulangan string,inputan
+
+/*
+Kapan digunakan?
+
+    Ketika jumlah perulangan belum pasti.
+
+    Digunakan untuk menunggu kondisi tertentu terpenuhi.
+
+    Cocok untuk loop berbasis logika (misalnya menunggu data dari server, user input, dll).
+    
+    
+    
+
+  */
