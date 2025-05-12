b3009c2dc83102574eb1cc17a81ea85d3635265b
Muhammad AMirul Muchminiin <121718601+amirul110@users.noreply.github.com>
2025-05-12 07:17:42 +0700

    Create 16-switch-statement.js

---
+++ Javascript Dasar/16-switch-statement.js
@@ -0,0 +1,17 @@
+let nilaiUjian = "C"
+
+switch(nilaiUjian){
+    case "A":
+        console.log("AND LULUS DENGAN GRADE A");
+        break;
+    case "B" :
+    case "C" :
+        console.log("ANDA LULUS DENGAN GRADE C")
+        break;
+    case "D" :
+        console.log("ANDA LULUS DENGAN GRADE D")
+        break;
+    default:
+        console.log("ANDA TIDAK LULUS")
+
+    }
