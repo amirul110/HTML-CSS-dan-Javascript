403ac41b929e4b96594a023a4cf37f13993e7a0c
Muhammad AMirul Muchminiin <121718601+amirul110@users.noreply.github.com>
2025-05-12 07:13:42 +0700

    Create array.js

---
+++ array.js
@@ -0,0 +1,20 @@
+const arrayNIM = [];
+
+arrayNIM.push(2249838,22833739,22775538,22123456);
+
+console.table(arrayNIM);
+
+console.log(arrayNIM.length); // 4, beriisi panjang array
+
+console.log(arrayNIM[2]);//dapatkan nlai dari indek ke 2
+
+console.log(arrayNIM[2] = 10); // mengubah data indek 2 jadi value 10
+
+console.table(arrayNIM);
+
+console.log(delete arrayNIM[2]); // meghapus index 2
+console.table(arrayNIM);
+
+
+arrayNIM[2] = 200;
+console.table(arrayNIM)
