13138103a59cb3873cef5d1e3afa81b362af8e01
Muhammad AMirul Muchminiin <121718601+amirul110@users.noreply.github.com>
2025-05-12 07:17:21 +0700

    Create 15-null.js

---
+++ Javascript Dasar/15-null.js
@@ -0,0 +1,11 @@
+let namaKedua = null;
+
+if (namaKedua === undefined) {
+    console.log("HELLO UNDEFINED");
+} else if (namaKedua === null) {
+    console.log("HELLO NULL");
+} else {
+    console.log(`Hello ${namaKedua}`)
+}
+
+console.log(typeof namaKedua )
