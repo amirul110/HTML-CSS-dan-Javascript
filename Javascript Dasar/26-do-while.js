d74eabde3a96c6f08beff58faa3bf4f80ce0620f
Muhammad AMirul Muchminiin <121718601+amirul110@users.noreply.github.com>
2025-05-13 06:41:09 +0700

    Create 26-do-while.js

---
+++ Javascript Dasar/26-do-while.js
@@ -0,0 +1,8 @@
+let baca = 1000;
+
+do { // selalu jalan minimal 1 kali, walau kondisi salah
+    console.log(`Perulangan ke ${baca}`);  // block perulangan
+    baca++
+}
+while (baca <= 10);  // kondisi 
+
