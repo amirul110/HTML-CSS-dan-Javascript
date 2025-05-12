36cbbd57074ec6621b3b93c5bb04bb564d94cc42
Muhammad AMirul Muchminiin <121718601+amirul110@users.noreply.github.com>
2025-05-12 07:20:17 +0700

    Create 19-ternary-operator.js

---
+++ Javascript Dasar/19-ternary-operator.js
@@ -0,0 +1,7 @@
+const nilai = 23;
+
+// ternary operator itu if else dalam 1 baris
+            // kondisi        nilai true            // nilai false
+const reward = nilai >= 23 ? "selamat anda lulus" : "silahkan coba lagi NANTIIIIIIIIIIIIII"
+
+console.log(` ${reward}`) //selamat anda lulus
