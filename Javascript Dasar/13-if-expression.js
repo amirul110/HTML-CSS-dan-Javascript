42d4b9ec42ecc8fcb65bd316f4257869e454c2c8
Muhammad AMirul Muchminiin <121718601+amirul110@users.noreply.github.com>
2025-05-12 07:16:49 +0700

    Create 13-if-expression.js

---
+++ Javascript Dasar/13-if-expression.js
@@ -0,0 +1,24 @@
+const nilaiUjian = 88;
+
+// if expression
+if (nilaiUjian => 88) {
+    console.log("ANda lulus") // akan di eksekusi jika nilaiUjian > 8 bernilai benar
+};
+
+
+ let UjianKuliah = 81;
+ // else expression
+if (UjianKuliah > 90) {
+    console.log("ANDAAAAAAAAAA LUULUUS")
+} else {
+    console.log("ANDA GAGAL COBA LAGIII")
+};
+
+let ujianRenang = 67;
+if (ujianRenang > 80) {
+    console.log("ANDA LULUS DENGAN GRADE B")
+} else if (ujianRenang > 60) {
+    console.log("ANDA LULUS DENGAN GRADE C")
+} else {
+    console.log("ANDA TIDAK LULUS")
+}
