aeb13f50076e67ac414a1ff2992b8d1b1dd26367
Muhammad AMirul Muchminiin <121718601+amirul110@users.noreply.github.com>
2025-05-14 08:22:13 +0700

    Update 29-for-in.js

--- Javascript Dasar/29-for-in.js
+++ Javascript Dasar/29-for-in.js
@@ -4,7 +4,9 @@
 FOR IN HANYA ITERASI PROPERTY ATAU INDEX
 
 ${person[property]} itu sama halnya dengan person["lastName"] hanya saja disini pada bagian person["lastName"] kita sudah tahu nama property apa yang ingin kita dapatkan nilainya
- dan sedangkan di ${person[property]} itu tidak ada tidak tahu nama property yang kita akses ,dan cara kerjanya adalah const property in person) berarti mengiterasi nama2 property di var person nah kalau sudah iterasi dan nilai tersebut disijmpan di const property maka seprti ${person["lastName"]} dan dari situ dapatkan nilai nya
+ dan sedangkan di ${person[property]} itu tidak ada tidak tahu nama property yang kita akses ,dan cara kerjanya adalah const property in person) 
+ berarti mengiterasi nama2 property  di var person nah kalau sudah iterasi dan nilai tersebut disijmpan di const property maka seprti ${person["lastName"]} dan dari situ dapatkan nilai nya
+
 
 
  */
