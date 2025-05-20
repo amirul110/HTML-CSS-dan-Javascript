d137a77c9ff2af2a0cadb60851c5e550816dc6f8
Muhammad AMirul Muchminiin <121718601+amirul110@users.noreply.github.com>
2025-05-21 06:30:06 +0700

    Update 42-func-generator.js

--- Javascript Dasar/42-func-generator.js
+++ Javascript Dasar/42-func-generator.js
@@ -1,18 +1,45 @@
 /*
 Generator function (function*) adalah jenis fungsi yang bisa berhenti di tengah jalan, menyimpan posisinya, dan dilanjutkan lagi nanti.
+
+
+tidak bisa diubah data nya
+tidak bisa diakses melalui index
+hanya mengambil data yang dibutuhkan
+
+Generator function (function* createNames()) disebut generator,
+tapi hasil pemanggilannya (createNames()) adalah iterator.
+
+setelah yield "EKo" dieksekusi maka akan lanjut 
 */ 
 function* createNames() {
-    yield "Eko"
-    yield "kurniawan"
-    yield "khannedy"
+    yield "Eko";         // <-- akan dieksekusi saat names.next() pertama
+    yield "kurniawan";   // <-- akan dieksekusi saat names.next() kedua
+    yield "khannedy";    // <-- akan dieksekusi saat names.next() ketiga
 }
 
-const names = createNames(); // names sbgai iterator 
+const names = createNames(); // iterator dibuat, belum jalan
 
-console.log(names.next());
-console.log(names.next());
-console.log(names.next());
-console.log(names.next());
+console.log(names.next());   // 🔥 Eksekusi dimulai → yield "Eko"
+// Output:
+// Masuk ke generator
+// { value: "Eko", done: false }
+
+console.log(names.next());   // 🔥 Lanjut dari titik terakhir (setelah yield "Eko")
+// Output:
+// Setelah Eko
+// { value: "Kurniawan", done: false }
+
+console.log(names.next());   // 🔥 Lanjut dari titik terakhir (setelah yield "Kurniawan")
+// Output:
+// Setelah Kurniawan
+// { value: "Khannedy", done: false }
+
+console.log(names.next());   // 🔥 Generator selesai
+// Output:
+// Setelah Khannedy
+// { value: undefined, done: true }
+
+
 
 function* buatGenap(value){
     for (let i = 0; i <= value; i++){
