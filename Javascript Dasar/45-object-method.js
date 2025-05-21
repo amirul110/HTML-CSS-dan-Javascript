a0d38e8142f9db45b223057c082e503d8a37108d
Muhammad AMirul Muchminiin <121718601+amirul110@users.noreply.github.com>
2025-05-22 05:06:42 +0700

    Create 45-object-method.js

---
+++ Javascript Dasar/45-object-method.js
@@ -0,0 +1,20 @@
+const person = {
+    name : "emo",
+    sayHello : function(name) {
+        console.log(`Hallo ${name}`)
+    }
+};
+
+person.sayHello("joko")
+
+
+// menambahkan method ke object
+// masih belum jelas
+const orang = {
+    nami : "sutiyad"
+};
+
+orang.sayHi = function(nami) {
+    console.log(`Hello ${nami}`)
+}
+orang.sayHi("hihi")
