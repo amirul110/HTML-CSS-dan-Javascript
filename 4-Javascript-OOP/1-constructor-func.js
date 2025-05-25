56965a2f7543cc2ac0478cddb1482719a861b25d
Muhammad AMirul Muchminiin <121718601+amirul110@users.noreply.github.com>
2025-05-25 11:40:04 +0700

    Update 1-constructor-func.js

--- 4-Javascript-OOP/1-constructor-func.js
+++ 4-Javascript-OOP/1-constructor-func.js
@@ -13,6 +13,7 @@ function Person(){ //disarankan huruf Capital huruf pertama nama class
 // 2. masunkan property firstName , lastName ke object baru karena this.
 // 3. setelah func person selesai,object baru yang telah terisi property disimpan di var eko
 const eko = new Person();
+// 1. mengisi object setelah object dideklarasikan
 eko.firstName = "ekoo"
 
 const budi = new Person();
@@ -22,3 +23,16 @@ budi.lastName = "glor";
 
 console.log(eko)
 console.log(budi);
+
+// 2. Mengisi Nilai Langsung dari Constructor lewat parameter
+function Orang(paramSatu, paramDua) {
+    this.namaPertama = paramSatu; // property namaPertama berii nilai yang berasal dar parameter namaPertama
+    this.namaKedua = paramDua; 
+}
+
+const OrangEko = new Orang("Eko", "Santoso");
+const OrangBudi = new Orang("Budi", "Glor");
+
+console.log(OrangEko);
+console.log(OrangBudi);
+
