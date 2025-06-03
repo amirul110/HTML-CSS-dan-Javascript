32572dc99005fe035aa7fee39ad7137f7b8c1838
Muhammad AMirul Muchminiin <121718601+amirul110@users.noreply.github.com>
2025-06-03 19:46:52 +0700

    Create 2-String-instace-method-properties.js

---
+++ 4-Javascript Standart Libray/2-String-instace-method-properties.js
@@ -0,0 +1,20 @@
+/*
+JavaScript membuang objek sementara (hasil autoboxing) segera setelah operasi selesai. 
+Tidak ada durasi tertentu — benar-benar instan, hanya digunakan untuk satu baris kode itu saja.
+
+*/
+
+
+const nama = "Eko Kurniawan Khanedy" ;
+
+console.log(nama.length); // 21  , property
+console.info(nama.toLocaleLowerCase()); // eko kurniawan khanedy  , instance method
+console.info(nama.toUpperCase()); // EKO KURNIAWAN KHANEDY
+const a = " ddd dd dnd"
+console.log(a.trim())
+
+const pecah = nama.split(" ")
+console.info(pecah); // [ 'Eko', 'Kurniawan', 'Khanedy' ] , instance method
+
+console.info(nama) // Eko Kurniawan Khanedy  , nilai tidak berubah  
+// instance method dan properties membuat data baru
