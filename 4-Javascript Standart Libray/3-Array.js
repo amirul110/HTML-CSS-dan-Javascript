1cb3f716398c1ff1529a0c7574bc6386906b6c07
Muhammad AMirul Muchminiin <121718601+amirul110@users.noreply.github.com>
2025-06-05 20:58:33 +0700

    Update 3-Array.js

--- 4-Javascript Standart Libray/3-Array.js
+++ 4-Javascript Standart Libray/3-Array.js
@@ -1,9 +1,17 @@
+/*
+forEach tidak bisa dihentikan dengan break atau return. Dia akan terus lanjut ke elemen berikutnya.
+
+// setiap  gunakan forEach pada array harus gunakan function callback
+for...of bisa dihentikan dengan break, cocok untuk kasus yang butuh keluar dari loop lebih awal.
+*/
+
 const array = ["Eko","Kurniawan","Khanedy"];
 
-array.forEach(function (nilai,indek){
+// parameter nilai = isi array , parameter indek = posisi index di array
+array.forEach(function (nilai,indek){ 
     console.info(`${indek} : ${nilai}`);
 })
 
 array.forEach((nilai, indek) => console.info(`${indek} : ${nilai}`)) ;
 
-array.forEach(nilai => console.log(nilai))
+array.forEach((nilai,indek) => console.log(nilai,indek))
