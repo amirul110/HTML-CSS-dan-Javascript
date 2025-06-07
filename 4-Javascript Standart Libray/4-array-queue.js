26b8ec31e023c02d6c32c43986dc5a3189d4e257
Muhammad AMirul Muchminiin <121718601+amirul110@users.noreply.github.com>
2025-06-07 11:58:27 +0700

    Update 4-array-queue.js

--- 4-Javascript Standart Libray/4-array-queue.js
+++ 4-Javascript Standart Libray/4-array-queue.js
@@ -1,9 +1,19 @@
+// GUNAKAN FIRST IN FIRST OUT
+// YANG MASUK DULU LANGSUNG DICETAK
 const queue = [];
 
-queue.push("Eko");
+queue.push("Eko"); // MENAMBAH DATA DIBELAKANG
 queue.push("Kurniawan");
 queue.push("kanedy");
 
+console.info(queue.shift()); // MENGAMBIL DATA DEPAN
 console.info(queue.shift());
 console.info(queue.shift());
-console.info(queue.shift());
+
+// console.info(queue[0]) juga bisa mencetak, 
+// shift() mengambil & menghapus
+console.info(queue.push("jarwo"))
+console.info(queue.push("jarwo")) // 2, mengembalikan jumlah data di array
+
+queue.push('akam')
+console.info(queue) // [ 'jarwo', 'jarwo', 'akam' ] , mencetak isi array
