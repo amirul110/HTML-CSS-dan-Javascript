f7223704c72a47f1eac90c0dfcc70fe62be2f65e
Muhammad AMirul Muchminiin <121718601+amirul110@users.noreply.github.com>
2025-06-09 07:40:00 +0700

    Create 10-object-assign.js

---
+++ 4-Javascript Standart Libray/10-object-assign.js
@@ -0,0 +1,13 @@
+const target = {firstName : "Eko"};
+const source = {lastName : "kanedy",middleName : "kurniawan",a : "aku"};
+
+//  property  object source ditambahkan     di object target
+
+Object.assign(target,source); //property urut milik target dahulu, baru milik source
+console.info(target); /* {
+  firstName: 'Eko',
+  lastName: 'kanedy',
+  middleName: 'kurniawan',
+  a: 'aku'
+}
+  */
