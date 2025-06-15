52ff896a2f86605152084c9d979f800f52db2829
Muhammad AMirul Muchminiin <121718601+amirul110@users.noreply.github.com>
2025-06-15 07:26:18 +0700

    Update 21-Regexp-method.js

--- 4-Javascript Standart Libray/21-Regexp-method.js
+++ 4-Javascript Standart Libray/21-Regexp-method.js
@@ -20,3 +20,11 @@ console.log(gantiSemua) // saya suka menonton olahraga dan menonton matematika
 const cerita = "apel;unta,dermawan*change";
 const pisah = cerita.split(/[;,*]/) // string dipotong berdasarkan ;,* 
 console.log(pisah); // [ 'apel', 'unta', 'dermawan', 'change' ]
+
+// gabungan regex + method
+const password = "abc123"
+const kuat = password.match(/[0-9]/g) && password.length >= 6; // password dengan karakter angka dan panjang minim 6 
+
+// gunakan operator ternary "?", hasilkan boolean
+// condition ? value_if_true : value_if_false
+console.log(kuat ? "password kuat" : "Password lemah") // password kuat
