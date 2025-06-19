b236a67072ffe59731e78d9b5f411cb2dd754031
Muhammad AMirul Muchminiin <121718601+amirul110@users.noreply.github.com>
2025-06-19 09:08:19 +0700

    Update 23-Reflect.js

--- 4-Javascript Standart Libray/23-Reflect.js
+++ 4-Javascript Standart Libray/23-Reflect.js
@@ -2,7 +2,9 @@
 /*
 Reflect adalah objek bantu (utility) di JavaScript yang digunakan untuk melakukan operasi terhadap objek secara fungsional — bukan dengan operator, tapi dengan fungsi.
 
+reflect merupakan object , fungsi2 statis untuk mengakses,mnyetel,memanipulasi object
 
+berupa object yang mempunyai fungsi2 statis seperti .has untuk mengecek apakah ada  property ini di object? kembalikan boolean.set untuk merubah nilai , get untuk dapatan data berdarasran propertynya
 */
 const people = {};
 
