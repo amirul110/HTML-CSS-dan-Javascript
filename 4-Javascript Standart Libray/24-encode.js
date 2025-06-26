6b507deb9294755989f59dd6b39814e2fd68c85a
Muhammad AMirul Muchminiin <121718601+amirul110@users.noreply.github.com>
2025-06-26 22:34:31 +0700

    Update 24-encode.js

--- 4-Javascript Standart Libray/24-encode.js
+++ 4-Javascript Standart Libray/24-encode.js
@@ -1,8 +1,40 @@
+/* Encoding adalah proses mengganti karakter tertentu dengan bentuk lain agar data dapat dikirim atau disimpan tanpa merusak struktur data.
+➡ Tujuan utamanya bukan menyembunyikan, tapi memastikan data tetap utuh dan terbaca oleh sistem.
+
+encodeURI()
+
+🟢 Fungsi: Meng-encode URL, tapi tetap mempertahankan karakter penting URI seperti: ; , / ? : @ & = + $ #
+ncodeURIComponent()
+
+encodeURIComponent()
+
+🟢 Fungsi: Meng-encode semua karakter khusus, cocok untuk bagian nilai parameter.
+
+
+decodeURI()
+
+🔄 Fungsi: Membalik hasil dari encodeURI() ke bentuk semula.
+
+decodeURIComponent()
+
+🔄 Fungsi: Membalik hasil dari encodeURIComponent() ke bentuk aslinya.
+*/
+
 const value = "Eko&Kurniawan=Khannedy;";
 const url = 'http://www.contoh.com/?name=';
 
 const encoded = encodeURIComponent(value);
-console.info(url + encoded);  // http://www.contoh.com/?name=Eko%26Kurniawan%3DKhannedy%3B
+console.info(url + encoded); // http://www.contoh.com/?name=Eko%26Kurniawan%3DKhannedy%3B
 
 const decoded = decodeURIComponent(encoded);
 console.info(url + decoded); // http://www.contoh.com/?name=Eko&Kurniawan=Khannedy;
+
+const value1 = "??/& & / 20";
+const hasilEncodeUri = encodeURI(value1);
+console.info("hasil encodeURI :",hasilEncodeUri); // hasil encodeURI : ??/&%20&%20/%2020  
+
+const decoded1 = decodeURI(value1)
+console.info("hasil decode value1 :",decoded1) // hasil decode value1 : ??/& & / 20   
+
+const decodedURIComponent = decodeURIComponent("Amirul%20%26%20Budi")
+console.info(decodedURIComponent) // Amirul & Budi
