d8fa854ccd6a92b25e2635a078d1955c23c86232
Muhammad AMirul Muchminiin <121718601+amirul110@users.noreply.github.com>
2025-06-27 15:09:13 +0700

    Update 4-class.js

--- 5- Javascript Modules/4-class.js
+++ 5- Javascript Modules/4-class.js
@@ -1,14 +1,16 @@
 export class Person {
     constructor(name) {
         this.jeneng = name // isi jeneng dari nilai parameter constructor , yaitu = Eko
-        this.kulo = null // belum diisi
+        this.kulo = null // belum diisi , kulo = "Madiun"
     }
 
     setKota(kota){
-        this.kulo = kota // kulo = "Madiun"
+        this.kulo = kota // kulo = "jakarta"
     }
 
     sayHello(motor){
+
+        // Hallo bang Joko , my name is Eko from jakarta 
         console.info(`Hallo bang ${motor} , my name is ${this.jeneng} from ${this.kulo} `)
     }
 }
