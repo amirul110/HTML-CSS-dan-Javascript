0df4a60f9224dab949c15efa1c469f4c3cf78764
Muhammad AMirul Muchminiin <121718601+amirul110@users.noreply.github.com>
2025-06-29 06:09:02 +0700

    Create 9_module.js

---
+++ 5- Javascript Modules/9_module.js
@@ -0,0 +1,14 @@
+export const firstName = "Eko";
+export const middleName = "Kurniawan"
+export const lastName = "Kanedy"
+
+export function hello() {
+    console.info("Hello from eko");
+}
+
+export class Mukena {
+    constructor(){
+        this.namaku = "Eko"
+    }
+}
+
