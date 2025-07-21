159244f445752596bced0ffd2725cb4fbd799992
Muhammad AMirul Muchminiin <121718601+amirul110@users.noreply.github.com>
2025-07-21 22:17:04 +0700

    Update 3_1-create-AJAX.js

--- 10 - Javascript Async/3_1-create-AJAX.js
+++ 10 - Javascript Async/3_1-create-AJAX.js
@@ -3,7 +3,6 @@ ikiajax.open('GET', 'api/hello.json');
 // ajax.open('GET', 'http://localhost:8080/api/helllo.json');
 
 ikiajax.send();
-
 /*
 AJAX (Asynchronous JavaScript and XML) adalah teknik untuk mengambil atau mengirim data ke server di belakang layar
 (tanpa me-refresh halaman web).
